f67fed6bb3075b460c8f41595f355f660e8d56da
Boris De Vloed <code@devloed.com>
2022-11-21 15:57:13 +0000

    improved performance, added data by value support

---
+++ eye.py
@@ -0,0 +1,66 @@
+import subprocess
+import os
+import time
+from pathlib import Path
+from helpers import generate_uuid, log
+
+
+class Eye:
+
+    def __init__(self) -> None:
+        self.data = []
+        self.queries = []
+        self.options = ['--nope', '--pass']
+        self.temp_files = []
+        self.temp_dir = '/tmp/'
+
+    def add_queries(self, *queries) -> None:
+        # pass outputs the full deductive closure, has to be removed when
+        # filtering with queries
+        if '--pass' in self.options:
+            self.options.remove('--pass')
+        self.queries.extend(*queries)
+
+    def add_data_by_reference(self, *data):
+        self.queries.extend(*data)
+
+    def add_data_by_value(self, data):
+        temp_file_name = (
+            f'{self.temp_dir}'
+            f'{time.strftime("%Y%m%d-%H%M%S")}-{generate_uuid()}.ttl'
+        )
+        temp_file = open(temp_file_name, "w")
+        temp_file.write(data)
+        temp_file.close()
+        self.temp_files.append(temp_file_name)
+        log(f'Created temp file {temp_file_name}')
+
+    def cleanup(self):
+        for file in self.temp_files:
+            log(f'Deleting {file}')
+            os.remove(file)
+
+    def serialize_command(self) -> str:
+        return (
+            ['eye'] +
+            self.data +
+            self.temp_files +
+            [v for file in self.queries for v in ('--query', file)] +
+            self.options
+        )
+
+    def reason(self, *args, **kwargs):
+        try:
+            process = subprocess.run(
+                self.serialize_command(),
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                encoding='utf-8',
+                check=True
+            )
+            return process.stdout, process.returncode
+        except subprocess.CalledProcessError as cpe:
+            log(cpe.stderr)
+            return cpe.stderr, cpe.returncode
+        finally:
+            self.cleanup()
--- web.py
+++ web.py
@@ -1,7 +1,7 @@
-from flask import request, stream_with_context, Response
-import os
+from flask import request, make_response
 from pathlib import Path
-from lib.eye import Eye
+from eye import Eye
+from helpers import error
 
 default_options = {
   'data': [],
@@ -11,50 +11,27 @@ default_options = {
 
 CONFIG_DIR = '/app/config/'
 
-
-@app.route("/hello")
-def hello():
-    return "Hello from the mu-python-template!"
-
-
-# @app.route("/", methods=['GET', 'POST'])
-def reason():
-    data = []
-    if 'data' not in request.values:
-        return 'No data in to reason upon', 400
-    else:
-        # data can be urls or ttl
-        data_content = request.values['data']
-        assert isinstance(data_content, str)
-        if data_content.startswith('http'):
-            data = data_content.split(',')
-        else:
-            pass  # TODO
-    eye = Eye()
-    return Response(stream_with_context(eye.reason()))
-
-
-@app.route("/", defaults={'path': None},methods=['GET', 'POST'])
-@app.route('/<path:path>', methods=['GET', 'POST'])
+@app.route("/reason/", defaults={'path': None}, methods=['GET', 'POST'])
+@app.route('/reason/<path:path>', methods=['GET', 'POST'])
 def reason_with_config(path):
     if 'data' not in request.values:
         return 'No data in to reason upon', 400
     eye = Eye()
-    if path is None:
-        return 'default'
-    else:
+
+    if path is not None:
         config = Path(f'{CONFIG_DIR}/{path}')
         if config.exists():
             eye.add_queries([query.resolve() for query in config.glob('*.n3q')])
-            eye.add_data_by_refernce([data.resolve() for data in config.glob('*.n3')])
+            eye.add_data_by_reference([data.resolve() for data in config.glob('*.n3')])
         else:
-            return 'Not found', 404
+            error(f'No config for {path}', status=404)
 
     data_content = request.values['data']
     assert isinstance(data_content, str)
     if data_content.startswith('http'):
-        eye.add_data_by_refernce(data_content.split(','))
+        eye.add_data_by_reference(data_content.split(','))
     else:
-        pass  # TODO
-    return Response(stream_with_context(eye.reason()))
+        eye.add_data_by_value(data_content)
 
+    data, code = eye.reason()
+    return make_response(data, 200 if not code else 400)
