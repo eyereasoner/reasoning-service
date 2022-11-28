3a171afad8cecba59d65b04ccaf35000960256c3
Boris De Vloed <code@devloed.com>
2022-11-28 14:11:05 +0000

    more verbose logging

--- web.py
+++ web.py
@@ -14,8 +14,11 @@ CONFIG_DIR = '/config/'
 @app.route("/reason/", defaults={'path': None}, methods=['GET', 'POST'])
 @app.route('/reason/<path:path>', methods=['GET', 'POST'])
 def reason_with_config(path):
+    log(f'{request.method} {path}')
     if 'data' not in request.values:
-        return 'No data in to reason upon', 400
+        msg = f'No data in to reason upon {path}'
+        log(f'400 {msg}')
+        return error(msg, status=400)
     eye = Eye()
 
     if path is not None:
@@ -24,7 +27,9 @@ def reason_with_config(path):
             eye.add_queries([query.resolve() for query in config.glob('*.n3q')])
             eye.add_data_by_reference([data.resolve() for data in config.glob('*.n3')])
         else:
-            return error(f'No config for {path}', status=404)
+            msg = f'No config for {path}'
+            log(f'404 {msg}')
+            return error(msg, status=404)
 
     data_content = request.values['data']
     assert isinstance(data_content, str)
@@ -34,4 +39,6 @@ def reason_with_config(path):
         eye.add_data_by_value(data_content)
 
     data, code = eye.reason()
-    return make_response(data, 200 if not code else 400)
+    http_code = 200 if not code else 400
+    log(f'{http_code} {path}')
+    return make_response(data, http_code)
