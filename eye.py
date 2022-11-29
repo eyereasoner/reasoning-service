1620575269551e90b0622fb7453446d3fa0a45ce
Boris De Vloed <code@devloed.com>
2022-11-29 11:33:50 +0000

    formatting and content type

--- eye.py
+++ eye.py
@@ -6,24 +6,23 @@ from helpers import generate_uuid, log
 
 
 class Eye:
-
     def __init__(self) -> None:
         self.data = []
         self.queries = []
         self.options = [
-            '--no-qnames',  # No prefixes, easier to partition triples later
-            '--quiet',      # No comments (timing, eye version and argments)
-            '--nope',       # No proof explanation
-            '--pass'        # Full deductive closure without filter/query
-            ]
+            "--no-qnames",  # No prefixes, easier to partition triples later
+            "--quiet",  # No comments (timing, eye version and argments)
+            "--nope",  # No proof explanation
+            "--pass",  # Full deductive closure without filter/query
+        ]
         self.temp_files = []
-        self.temp_dir = '/tmp/'
+        self.temp_dir = "/tmp/"
 
     def add_queries(self, *queries) -> None:
         # pass outputs the full deductive closure, has to be removed when
         # filtering with queries
-        if '--pass' in self.options:
-            self.options.remove('--pass')
+        if "--pass" in self.options:
+            self.options.remove("--pass")
         self.queries.extend(*queries)
 
     def add_data_by_reference(self, *data):
@@ -31,27 +30,26 @@ class Eye:
 
     def add_data_by_value(self, data):
         temp_file_name = (
-            f'{self.temp_dir}'
-            f'{time.strftime("%Y%m%d-%H%M%S")}-{generate_uuid()}.ttl'
+            f"{self.temp_dir}" f'{time.strftime("%Y%m%d-%H%M%S")}-{generate_uuid()}.ttl'
         )
         temp_file = open(temp_file_name, "w")
         temp_file.write(data)
         temp_file.close()
         self.temp_files.append(temp_file_name)
-        log(f'Created temp file {temp_file_name}')
+        log(f"Created temp file {temp_file_name}")
 
     def cleanup(self):
         for file in self.temp_files:
-            log(f'Deleting {file}')
+            log(f"Deleting {file}")
             os.remove(file)
 
     def serialize_command(self) -> str:
         return (
-            ['swipl', '-x', '/usr/local/lib/eye.pvm', '--'] +
-            self.data +
-            self.temp_files +
-            [v for file in self.queries for v in ('--query', file)] +
-            self.options
+            ["swipl", "-x", "/usr/local/lib/eye.pvm", "--"]
+            + self.data
+            + self.temp_files
+            + [v for file in self.queries for v in ("--query", file)]
+            + self.options
         )
 
     def reason(self, *args, **kwargs):
@@ -60,8 +58,8 @@ class Eye:
                 self.serialize_command(),
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
-                encoding='utf-8',
-                check=True
+                encoding="utf-8",
+                check=True,
             )
             log(process.stderr)
             return process.stdout, process.returncode
--- web.py
+++ web.py
@@ -3,40 +3,43 @@ from pathlib import Path
 from eye import Eye
 from helpers import error, log
 
-CONFIG_DIR = '/config/'
+CONFIG_DIR = "/config/"
 
-@app.route("/reason/", defaults={'path': None}, methods=['GET', 'POST'])
-@app.route('/reason/<path:path>', methods=['GET', 'POST'])
+
+@app.route("/reason/", defaults={"path": None}, methods=["GET", "POST"])
+@app.route("/reason/<path:path>", methods=["GET", "POST"])
 def reason_with_config(path):
-    log(f'{request.method} {path}')
-    if 'data' not in request.values:
-        msg = f'No data in to reason upon {path}'
-        log(f'400 {msg}')
+    log(f"{request.method} {path}")
+    if "data" not in request.values:
+        msg = f"No data in to reason upon {path}"
+        log(f"400 {msg}")
         return error(msg, status=400)
     eye = Eye()
 
     if path is not None:
-        config = Path(f'{CONFIG_DIR}/{path}')
+        config = Path(f"{CONFIG_DIR}/{path}")
         if config.exists():
-            queries = [query.resolve() for query in config.glob('*.n3q')]
+            queries = [query.resolve() for query in config.glob("*.n3q")]
             if queries:
                 eye.add_queries(queries)
-            eye.add_data_by_reference(
-                [data.resolve() for data in config.glob('*.n3')]
-            )
+            eye.add_data_by_reference([data.resolve() for data in config.glob("*.n3")])
         else:
-            msg = f'No config for {path}'
-            log(f'404 {msg}')
+            msg = f"No config for {path}"
+            log(f"404 {msg}")
             return error(msg, status=404)
 
-    data_content = request.values['data']
+    data_content = request.values["data"]
     assert isinstance(data_content, str)
-    if data_content.startswith('http'):
-        eye.add_data_by_reference(data_content.split(','))
+    if data_content.startswith("http"):
+        eye.add_data_by_reference(data_content.split(","))
     else:
         eye.add_data_by_value(data_content)
 
     data, code = eye.reason()
-    http_code = 200 if not code else 400
-    log(f'{http_code} {path}')
-    return make_response(data, http_code)
+
+    if code:
+        return error(data, status=500)
+    else:
+        response = make_response(data)
+        response.mimetype = "text/turtle"
+        return response
