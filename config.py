b587ee2e4151a2ddfe0102813f41bc0bf4887d93
Boris De Vloed <code@devloed.com>
2022-12-19 12:13:53 +0000

    support explict keep_temp_files config

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 CONFIG_DIR = os.getenv("CONFIG_DIR") or "/config/"
 
-KEEP_TEMP_FILES = False
-if isinstance(os.getenv('MODE'), str):
-    KEEP_TEMP_FILES = os.getenv("MODE").lower() == 'development'
+KEEP_TEMP_FILES = os.getenv("KEEP_TEMP_FILES") or False
+if not KEEP_TEMP_FILES and isinstance(os.getenv("MODE"), str):
+    KEEP_TEMP_FILES = os.getenv("MODE").lower() == "development"
